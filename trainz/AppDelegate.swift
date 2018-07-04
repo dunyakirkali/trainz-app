c98adb0e30a89d5046814c64c54a7821f9f4df21
Dunya Kirkali <dunyakirkali@gmail.com>
2018-07-04 08:04:16 +0200

    Initial commit

---
+++ trainz/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  trainz
+//
+//  Created by Dunya Kirkali on 04/07/2018.
+//  Copyright © 2018 Dunya Kirkali. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ trainz/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  trainz
+//
+//  Created by Dunya Kirkali on 04/07/2018.
+//  Copyright © 2018 Dunya Kirkali. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
