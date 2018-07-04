5ceb0a5553798259a4b7e5e31cad4f6cc64f8b14
Dunya Kirkali <dunyakirkali@gmail.com>
2018-07-05 00:24:51 +0200

    Add MapBox

--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
-platform :ios, '10.0'
+platform :osx, '10.11'
 use_frameworks!
 inhibit_all_warnings!
 
-target 'trainz' do
+target "trainz" do
+  pod "Mapbox-macOS-SDK"
 end
--- trainz/ViewController.swift
+++ trainz/ViewController.swift
@@ -7,17 +7,18 @@
 //
 
 import Cocoa
-import MapKit
+import Mapbox
 
 class ViewController: NSViewController {
-    
+
     let topSpeed: Double = 500 // kmph
 
+    @IBOutlet weak var speedLabel: NSTextField!
     @IBOutlet weak var hornButton: NSButton!
     @IBOutlet weak var followToggle: NSButton!
-    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var mapView: MGLMapView!
     @IBOutlet weak var speedSlider: NSSlider!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,6 +26,9 @@ class ViewController: NSViewController {
         setupSlider()
         setupToggle()
         setupHorn()
+        
+        let center = CLLocationCoordinate2D(latitude: 19.820689, longitude: -155.468038)
+        mapView.setCenter(center, animated: false)
     }
 
     override var representedObject: Any? {
@@ -40,6 +44,7 @@ class ViewController: NSViewController {
     private func setupSlider() {
         speedSlider.target = self
         speedSlider.action = #selector(ViewController.valueChanged(_:))
+        speedSlider.doubleValue = 0.0
     }
     
     private func setupToggle() {
@@ -53,17 +58,27 @@ class ViewController: NSViewController {
     }
 }
 
-extension ViewController: MKMapViewDelegate {
-
+extension ViewController: MGLMapViewDelegate {
+    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
+        // Wait for the map to load before initiating the first camera movement.
+        
+        // Create a camera that rotates around the same center point, rotating 180°.
+        // `fromDistance:` is meters above mean sea level that an eye would have to be in order to see what the map view is showing.
+        let camera = MGLMapCamera(lookingAtCenter: mapView.centerCoordinate, fromDistance: 4500, pitch: 15, heading: 180)
+        
+        // Animate the camera movement over 5 seconds.
+        mapView.setCamera(camera, withDuration: 5, animationTimingFunction: CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut))
+    }
 }
 
 extension ViewController {
     @objc func valueChanged(_ sender: NSSlider) {
-        print(scale(speed: sender.doubleValue))
+        let scaledValue: Int = scale(speed: sender.doubleValue)
+        speedLabel.stringValue = "\(scaledValue) km/h"
     }
-    
-    private func scale(speed: Double) -> Double {
-        return speed / 100.0 * topSpeed
+
+    private func scale(speed: Double) -> Int {
+        return Int(speed / 100.0 * topSpeed)
     }
 }
 
